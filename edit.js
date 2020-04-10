1aff20a167b4b88ecd19267a475b96266192fd18
namekinggamerboy <53903319+namekinggamerboy@users.noreply.github.com>
2020-04-10 09:05:37 +0530

    Create edit.js

---
+++ edit.js
@@ -0,0 +1,61 @@
+function submit() {
+    if (!document.getElementById('botid').value)
+        return flash(document.getElementById('botid'))  
+  if (!document.getElementById('mcolor').value)
+        return flash(document.getElementById('mcolor')) 
+  if (!document.getElementById('mimage').value)
+        return flash(document.getElementById('mimage'))
+    if (!document.getElementById('prefix').value)
+        return flash(document.getElementById('prefix'))
+    if (!document.getElementById('short').value)
+        return flash(document.getElementById('short'))
+     if (!document.getElementById('sinvite').value)
+        return flash(document.getElementById('sinvite'))
+    if (!document.getElementById('longdesc').value)
+        return flash(document.getElementById('longdesc'))
+
+    let data = {
+        token: localStorage.getItem('token'),
+        id: document.getElementById('botid').value,
+        prefix: document.getElementById('prefix').value,
+        short: document.getElementById('short').value,
+        invite: document.getElementById('invite').value,
+        owners: document.getElementById('owners').value,
+           sinvite: document.getElementById('sinvite').value,
+        long: document.getElementById('longdesc').value,  
+      mimage: document.getElementById('mimage').value,
+      mcolor: document.getElementById('mcolor').value
+       };
+    var url = `${window.location.origin}/modify`;
+    var form = $(`<form action="${url}" method="post">
+<input type="text" name="token" value="${data.token}" />
+<input type="text" name="id" value="${data.id}" />
+<input type="text" name="prefix" value="${data.prefix}" />
+<input type="text" name="short" value="${data.short}" />
+<input type="text" name="sinvite" value="${data.sinvite}" />
+<input type="text" name="invite" value="${data.invite}" />
+<input type="text" name="mimage" value="${data.mimage}" />
+<input type="text" name="mcolor" value="${data.mcolor}" />
+<input type="text" name="owners" value="${data.owners}" />
+<textarea name="long" value="${data.long}">${data.long}</textarea>
+</form>
+`);
+    $('body').append(form);
+    form.submit();
+}
+
+function flash(element) {
+    element.scrollIntoView();
+    element.style.border = "2px solid #ff0000";
+    element.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+    setTimeout(() => {
+        element.style.backgroundColor = "rgba(0, 0, 0, 0)";
+        element.style.border = "1px solid #888";
+    }, 600)
+}
+
+document.body.onload = () => {
+    let botId = location.href.split(location.host)[1].replace('/edit/', '').replace('/', '');
+    document.getElementById('auth').href = `/api/auth/${botId}?token=${localStorage.token}`;
+    document.getElementById('reset').href = `/api/auth/reset/${botId}?token=${localStorage.token}`;
+}
